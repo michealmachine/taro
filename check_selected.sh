f035f5a16e2057c0e2e558af1c369fda77f91d73
Double2and9 <micheal0machine@gmail.com>
2026-04-15 00:14:11 +0800

    chore: remove temp scripts, ignore *.sh

--- check_selected.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run --rm -v ~/taro/data:/data alpine sh -c 'apk add -q sqlite && sqlite3 /data/taro.db "SELECT substr(r.title,1,60), r.resolution, r.seeders FROM resources r JOIN entries e ON r.id=e.selected_resource_id WHERE e.title='"'"'Shingeki no Kyojin'"'"' LIMIT 2;"'
